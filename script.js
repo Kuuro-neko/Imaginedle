0cb7b460b5bd841f6de598aec976cf21d7b43c23
Gilles Gonzalez Oropeza <gilles.gonzalezoropeza@gmail.com>
2026-05-05 17:03:52 +0200

    dsqddsq

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const MUCH_HIGHER = 'much_higher';
 const MUCH_THRESHOLD = 5;
 
 const csvPath = 'data.csv';
-const urlForResultShare = 'https://kuuro-neko.github.io/imaginedle/';
+const urlForResultShare = 'https://kuuro-neko.github.io/Imaginedle/';
 const fieldLength = 5;
 const triesToHint = 5;
 
